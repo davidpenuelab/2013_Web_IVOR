dbb5fe2122c4db85b296ecd1711036b90d678908
davidpenuela <davidpenuelab@gmail.com>
2014-03-15 16:56:46 +0100

    first commit

---
+++ whatisuji.info/assets/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){    
+    $(".homeVimeo").fitVids();
+	loadAll();
+
+    updateResize();
+    $(window).resize(function(){
+        updateResize();
+    });
+    
+    $('#spyme').on('activate.bs.scrollspy', function () {
+		console.log('hello');
+  	})
+  	
+});
+
+function updateResize(){
+    $('.section').css('min-height',$(window).height()+'px')
+    setTimeout(function(){centerVideo();},1000);
+}
+
+function centerVideo(){
+	console.log("VIDEO "+$('#video').height());
+	console.log("WINDOW"+$(window).height())
+	var margin_vid = $(window).height()/2 - $('#video').height()/2;
+	$('#video').css('margin-top',margin_vid+'px');
+}
+function loadAll(){
+	$('#load0').fadeIn(500); // video first
+
+	setTimeout(function(){$('#load1').fadeIn(1000)},1000);	
+	setTimeout(function(){$('#load2').fadeIn(1000)},1000);	
+	setTimeout(function(){$('#load3').fadeIn(1000)},1000);	
+	setTimeout(function(){$('#load4').fadeIn(1000)},1000);
+	setTimeout(function(){$('#load5').fadeIn(1000)},1000);	
+}
